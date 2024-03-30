ec66aa36887289ca15eb3f5d35105f6dca8d1dcd
ana.olaru <andreea.olaru>
2024-03-30 14:08:36 +0200

    add back app folder

---
+++ app/js/resources/ro.js
@@ -0,0 +1,109 @@
+sofia.resources['ro'] = {
+	"translation": {
+		"name": "Română",
+		"menu": {
+			"search": {
+				"placeholder": "Căutare"
+			},
+			"config": {
+				"font": "Font",
+				"settings": "Setări",
+				"tools": "Instrumente"
+			},
+			"reset": "Restabilire",
+			"themes": {
+				"default": "Normal",
+				"sepia": "Sepia",
+				"dark": "Lumină scăzută"
+			},
+			"labels": {
+				"about": "Despre",
+				"addwindow": "Adăugă fereastră",
+				"settings": "Setări",
+				"feedback": "Opinie",
+				"options": "Opțiuni"
+			},
+			"feedback": {
+				"name": "Nume",
+				"email": "E-mail",
+				"comments": "Comentariile tale",
+				"feature": "Caracteristică Sugestie",
+				"bug": "Raportează",
+				"other": "Alte",
+				"send": "Trimite",
+				"thankyou": "Îți mulțumim pentru sugestii"
+			}
+		},
+		"plugins": {
+			"visualfilters": {
+				"button": "Filtre vizuale",
+				"title": "Filtre vizuale",
+				"newfilter": "Filtru nou",
+				"strongsnumber": "# Îngroșat",
+				"morphology": "Morfologie",
+				"style": "Stil"
+			},
+			"eng2p": {
+				"button": "Engleză, persoana a 2-a plural",
+				"title": "Engleză, persoana a 2-a plural"
+			},
+			"lemmapopup": {
+				"findalloccurrences": "Găsește toate ocurențele (aproximativ __număr__)"
+			}
+		},
+		"windows": {
+			"bible": {
+				"label": "Biblie",
+				"filter": "Filtru ...",
+				"ot": "Vechiul Testament",
+				"nt": "Noul Testament",
+				"dc": "Deuterocanonice Cărți",
+				"more": "Mai mult",
+				"less": "Mai puțin",
+				"recentlyused": "Folosit recent",
+				"languages": "Limbi",
+				"countries": "Țări"
+			},
+			"commentary": {
+				"label": "Comentariu"
+			},
+			"map": {
+				"label": "Hărți",
+				"placeholder": "Căutare ..."
+			},
+			"search": {
+				"label": "Căutare",
+				"placeholder": "Căutare",
+				"button": "Căutare",
+				"results": "Rezultate",
+				"verses": "Versete",
+				"options": "Opțiuni de căutare"
+			},
+			"media": {
+				"label": "Mass-media"
+			},
+			"notes": {
+				"label": "Notițe"
+			},
+			"audio": {
+				"options": "Opțiuni audio",
+				"synctext": "Sincronizare text (beta)",
+				"autoplay": "Autoredare următorul",
+				"drama": "Dramă",
+				"nondrama": "Non-dramă"
+			},
+			"parallel": {
+				"label": "Paralele",
+				"loading": "Se încarcă ...",
+				"showall": "Arată tot",
+				"hideall": "Ascunde tot"
+			},
+			"comparison": {
+				"label": "Comparație"
+			}
+		},
+		"names": {
+			"en": "Romanian"
+		}
+	}
+}
